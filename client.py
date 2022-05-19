81e3d28f4f371d84541dadac694d11262047c993
kaihr <lyhkhai@gmail.com>
2022-05-19 16:16:47 +0700

    Update
    - Request user input's for ip address and port number
    - Move `display_all()` to `client_utils.py`
    - Add a statement to handle the case where the user try to request data of a person that does not exist

--- client.py
+++ client.py
@@ -4,28 +4,7 @@
     - Login, signup for client
 ''' 
 import socket
-from client_utils import *
-
-def display_all(connection):
-    data = ask_phone_book_data(connection)
-    for item in data:
-        print(item)
-    
-    print('='*40)
-    while True:
-        user_input = input('>> ')
-        if user_input == 'back':
-            break        
-        
-        if user_input == 'download thumbnails':
-            receive_thumbnails(connection)
-            continue
-        
-        token = user_input.split()
-        
-        if token[0] == 'query':
-            ask_person_data(connection, token[1])
-            
+from client_utils import *            
         
 if __name__ == '__main__':
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP
@@ -33,7 +12,16 @@ if __name__ == '__main__':
     print(cfg)
 
     print("[CLIENT SIDE]")
-    client.connect((cfg['HOST'], cfg['SERVER_PORT']))
+    
+    while True:
+        ip_addr = input('ip address: ')
+        port_num = int(input('port number: '))
+        try:
+            client.connect((ip_addr, port_num))
+            break
+        except:
+            print('Connection failed, please try again')
+    
     print('Client address:', client.getsockname())
     print('='*40)
     welcome_msg = client.recv(1024).decode(cfg['FORMAT'])
